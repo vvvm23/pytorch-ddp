d4066f127709be599dc2ffbd0a20ef066ee64c62
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-08-25 18:21:36 +0100

    initial code commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+import os
+import argparse
+
+if __name__ == '__main__':
+    pass
