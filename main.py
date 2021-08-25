683f8a66b574bf885ec3a37c81f6a24818e12f02
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2021-08-25 18:31:22 +0100

    adds simple convnet

--- main.py
+++ main.py
@@ -7,5 +7,23 @@ import torch.multiprocessing as mp
 import os
 import argparse
 
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+        )
+        self.fc = nn.Linear(32*7*7, 10)
+
+    def forward(self, x):
+        return self.fc(self.conv2(self.conv1(x)))
+
 if __name__ == '__main__':
     pass
